f9c7bc1dea66b791357379d42b30cb33595cb913
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-02-06 23:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	 let date11 = new Date(date1);
+let date22 = new Date(date2);
+
+let Difference_In_Time =
+	date22.getTime() - date11.getTime();
+let Difference_In_Days =
+	Math.round
+		(Difference_In_Time / (1000 * 3600 * 24));
+
+
+return Difference_In_Days;
 };
 
 // Do not change the code below.
